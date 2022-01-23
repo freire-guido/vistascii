4e120f89401d37f096b1ba33e07f12f9c542f0f8
freire-guido <freireguido@outlook.com>
2022-01-23 09:21:24 -0300

    working example

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 #include "include/vistascii.h"
 
 int main() {
-    Vec3 v = {0, 1, 2};
-    va::VertexEntity vex = {v};
-    vex.add(Vec3(0, 3, 2));
-    cross(v, Vec3(1,0,0));
+    va::VertexEntity vex = {{1, 0, 2}, {10, 0, 2}}; 
+    va::VertexRenderer ver({vex});
+    ver.render();
+    getch();
+    endwin();
     return 0;
 }
