1cdde01301bed5d2a122be87991faf6708899efa
freire-guido <freireguido@outlook.com>
2022-01-25 11:11:23 -0300

    zbuffer

--- src/vistascii.cpp
+++ src/vistascii.cpp
@@ -8,8 +8,8 @@ char getDepthChar(int d) {
     return charMap[d / 2];
 }
 
-Vec3 persproject(Vec3 v, Vec3 n, float d) {
-    Vec3 p = v * d / dot(v, n) - normalize(n);
+Vec3 persproject(Vec3 v, Vec3 n, float dist) {
+    Vec3 p = v * dist / dot(v, n) - normalize(n);
     v.x = dot(p, {1, 0, -n.x});
     v.y = dot(p, {0, 1, -n.y});
     v.z = dot(v, normalize(n));
@@ -17,7 +17,7 @@ Vec3 persproject(Vec3 v, Vec3 n, float d) {
 }
 
 void va::VertexRenderer::render() {
-    std::vector<std::string> window(height, std::string(width, getDepthChar(-1)));
+    std::vector<std::vector<float>> window(height, std::vector<float>(width, -1));
     for (VertexEntity& entity: entities) {
         for (int i = 1; i <= entity.vertexes.size(); i++) {
             if (dot(entity.vertexes[i-1], camera_pos) > 0 || dot(entity.vertexes[i % entity.vertexes.size()], camera_pos) > 0) {
@@ -33,14 +33,20 @@ void va::VertexRenderer::render() {
                 for (int x = projectionA.x; x <= projectionB.x; x++){
                     int y = dy / dx * (x - projectionA.x) + projectionA.y;
                     if (0 <= x && x < width && 0 <= y && y < height) {
-                        window[y][x] = getDepthChar(projectionA.z + (projectionB.z - projectionA.z)*(x - projectionA.x)/(projectionB.x - projectionA.x));
+                        // Z buffer
+                        float depth = projectionA.z + (projectionB.z - projectionA.z)*(x - projectionA.x)/(projectionB.x - projectionA.x);
+                        if (depth < window[y][x] || window[y][x] == -1) {
+                            window[y][x] = depth;
+                        }
                     }
                 }
             }   
         }
     }
     for (int row = 0; row < window.size(); row++) {
-        mvaddstr(row, 0, window[row].c_str());
+        for (int col = 0; col < window[row].size(); col++) {
+            mvaddch(row, col, getDepthChar(window[row][col]));
+        }
     }
     refresh();
 }
