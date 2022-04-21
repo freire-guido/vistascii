19fde5d5573ee03487da21cd1f2f1eae37ce4ee4
freire-guido <freireguido@outlook.com>
2022-04-21 20:25:03 -0300

    shard demo

--- main.cpp
+++ main.cpp
@@ -1,23 +1,28 @@
 #include "include/vistascii.h"
+#include <iostream>
 
+using namespace va;
 int main() {
-    va::VertexRenderer ver({va::VertexEntity("./data/tank")});
-    ver.entities[0].move({0,0,3});
+    va::VertexRenderer ver({va::VertexEntity("./data/shard")}, {0,1,0}, 10);
+    ver.entities[0].move({0, 10, -2});
     Vec3 move;
     while (true) {
         ver.render();
         int key = getch();
         if (key == 'w') {
-            move = {0, 0.1, 0};
+            move = {0, 0.5, 0};
         } else if (key == 's') {
-            move  = {0, -0.1, 0};
+            move  = {0, -0.5, 0};
         } else if (key == 'd') {
-            move = {0.1, 0, 0};
+            move = {0.5, 0, 0};
         } else if (key == 'a') {
-            move = {-0.1, 0, 0};
+            move = {-0.5, 0, 0};
+        } else if (key == 'r') {
+            move = {0, 0, 0.5};
+        } else if (key == 'f') {
+            move = {0, 0, -0.5};
         }
-        ver.camera_pos += move;
-        ver.camera_pos /= ver.camera_pos.length();
+        ver.entities[0].move(move);
     }
     return 0;
 }
